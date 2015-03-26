bda000c7dc591e49ece1b2382d035a1249530137
Arrio Farugie <arrio.farugie@gmail.com>
2015-03-26 17:57:08 -0500

    add geometry_calculator

--- geometry_calculator/geometry_calculator.rb
+++ geometry_calculator/geometry_calculator.rb
@@ -1,17 +1,17 @@
   def display_menu
-    puts "Geometry Calculate\n" +
+    puts "Geometry Calculater\n" +
       "1. Calculate the area of a Circle\n" +
-      "2. Calculate the area of a Rectangl\n" +
+      "2. Calculate the area of a Rectangle\n" +
       "3. Calculate the area of a Triangle\n" +
       "4. Exit"
   end
 
-  def get_user_input
+  def user_input
     print "Enter your choice (1-4)\n"
     gets.chop
   end
 
-  def menu_selecter(choice)
+  def menu_select(choice)
     choices = {
       "1" => "calculate_area_of_circle",
       "2" => "calculate_area_of_rectangle",
@@ -24,32 +24,38 @@
   end
 
 class Geometry
-  def calculate_area_of_circle()
-    "Enter radius of circle"
+  def calculate_area_of_circle
+    puts "Enter radius of circle"
     radius = gets.to_f
-    Math::PI * (radius ** 2)
+
+    area = printf("total area is %.4f",Math::PI * (radius ** 2))
   end
 
-  def calculate_area_of_rectangle()
-    "enter length of rectangle"
+  def calculate_area_of_rectangle
+    puts "enter length of rectangle"
     length = gets.to_f
-    "enter width of rectangle"
+
+    puts "enter width of rectangle"
     width = gets.to_f
 
     area =  length * width
     "total area is #{area}"
   end
 
-  def calculate_area_of_triangle()
-    "enter size of triangle base"
+
+  def calculate_area_of_triangle
+    puts "enter size of triangle base"
     base = gets.to_f
-    "enter height of rectangle"
+
+    puts "enter height of rectangle"
     height = gets.to_f
 
     area = base * height * 0.5
-    "total area is #{area}"
+    puts "total area is #{area}"
   end
 end 
     
-display_menu
-puts menu_selecter get_user_input
+while (true)
+  display_menu
+  puts menu_select user_input
+end
